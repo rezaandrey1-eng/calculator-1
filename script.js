5c915112bca666e6168ab8d2cb69f1a288ef0238
rezaandrey1-eng <rezaandrey1@gmail.com>
2026-06-24 08:55:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let display = document.getElementById("display");
+
+
+function append(value){
+    display.value += value;
+}
+
+
+function clearDisplay(){
+    display.value = "";
+}
+
+
+function deleteNumber(){
+    display.value = display.value.slice(0,-1);
+}
+
+
+function calculate(){
+
+    try{
+
+        display.value = eval(display.value);
+
+    }
+
+    catch{
+
+        display.value = "Error";
+
+    }
+
+}
